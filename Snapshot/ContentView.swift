1fd955529adfa7a8cedac6b54656e400e1f839ed
Zheng <ahzzheng@gmail.com>
2021-04-30 10:45:34 -0700

    Initial commit

---
+++ Snapshot/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Snapshot
+//
+//  Created by Zheng on 4/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension View {
+    func snapshot() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        let view = controller.view
+
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
+    }
+}
---
+++ Snapshot/SnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotApp.swift
+//  Snapshot
+//
+//  Created by Zheng on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
